0fa5f8d3dfd402399888c80fba41978e654e3c0e
K4U4dev <okaua057.cod@gmail.com>
2025-02-23 23:49:47 -0300

    feat: add dependency injection manager class for repositories

---
+++ MeusAnimesAPI/Extensions/DependencyInjection/Repository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Interfaces.Repositories;
+using Infrastructure.Repositories;
+
+namespace MeusAnimesAPI.Extensions.DependencyInjection;
+
+public static class Repository
+{
+    public static IServiceCollection AddRepositoryDependencies(this IServiceCollection services)
+    {
+        services.AddScoped<IAnimeRepository, AnimeRepository>();
+        return services;
+    }
+}
