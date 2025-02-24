60c8bc35142d65d192f9b5869dcd32eeb4aa76d6
K4U4dev <okaua057.cod@gmail.com>
2025-02-23 23:35:33 -0300

    feat: add initial migrations

---
+++ Infrastructure/Migrations/20250223202626_InitialMigration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Anime",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    diretor = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    resumo = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Anime__3213E83FC77C470D", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Anime");
+        }
+    }
+}
