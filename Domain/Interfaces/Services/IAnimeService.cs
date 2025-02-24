d9e053b8b36e4a484100b49e35e6de0d1c00964b
K4U4dev <okaua057.cod@gmail.com>
2025-02-23 23:37:11 -0300

    feat: add service and repository interfaces

---
+++ Domain/Interfaces/Services/IAnimeService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces.Services;
+
+public interface IAnimeService
+{
+    Task<Anime> CreateAsync(Anime anime);
+    Task<Anime> UpdateAsync(Anime anime);
+    Task<bool> DeleteAsync(int id);
+    Task<IEnumerable<Anime?>> GetAllAsync();
+    Task<Anime?> GetByIdAsync(int id);
+    Task<IEnumerable<Anime?>> GetByNameAsync(string nameAnime);
+    Task<IEnumerable<Anime?>> GetByDirectorAsync(string nameDirector);
+}
