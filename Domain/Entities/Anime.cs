d09e2806a9746c04abfe8556b935f77d8c56a4cd
K4U4dev <okaua057.cod@gmail.com>
2025-02-23 23:14:08 -0300

    feat: add context class and scaffold table

---
+++ Domain/Entities/Anime.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities
+{
+    public class Anime
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; } = null!;
+
+        public string? Diretor { get; set; }
+
+        public string? Resumo { get; set; }
+    }
+}
---
+++ Infrastructure/Context/AnimeDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+using Infrastructure.Configurations;
+
+namespace Infrastructure.Context;
+
+public partial class AnimeDbContext : DbContext
+{
+    public AnimeDbContext()
+    {
+    }
+
+    public AnimeDbContext(DbContextOptions<AnimeDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Anime> Animes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AnimeConfiguration).Assembly);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.EnableSensitiveDataLogging();
+    }
+}
